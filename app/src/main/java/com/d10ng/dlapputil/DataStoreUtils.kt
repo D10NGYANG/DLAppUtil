b15163791163a875e14e2709dec0da9b4d92e46b
D10NG <1209842418@qq.com>
2021-08-27 11:24:54 +0800

    更新库；

--- app/src/main/java/com/d10ng/dlapputil/DataStoreUtils.kt
+++ app/src/main/java/com/d10ng/dlapputil/DataStoreUtils.kt
@@ -3,11 +3,13 @@ package com.d10ng.dlapputil
 import android.content.Context
 import com.d10ng.applib.system.dataStoreRead
 import com.d10ng.applib.system.dataStoreWrite
+import com.d10ng.stringlib.decrypt
+import com.d10ng.stringlib.encrypt
 
 suspend fun Context.readSaveName(): String {
-    return dataStoreRead(SPF_SAVE_NAME)
+    return dataStoreRead<String>(SPF_SAVE_NAME).decrypt()
 }
 
 suspend fun Context.writeSaveName(value: String) {
-    dataStoreWrite(SPF_SAVE_NAME, value)
+    dataStoreWrite(SPF_SAVE_NAME, value.encrypt())
 }
